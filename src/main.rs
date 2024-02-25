eeab12a9c56078b30d3c1aa607041cc57e61f19b
shayan <shayan.workspace@gmail.com>
2024-02-25 10:59:48 +0500

    fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn fibonacci(n: u32) -> u32 {
+    if n <= 1 {
+        return n;
+    }
+
+    fibonacci(n - 1) + fibonacci(n - 2)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 10;
+    let result = fibonacci(n);
+    println!("The {}th Fibonacci number is: {}", n, result);
 }
